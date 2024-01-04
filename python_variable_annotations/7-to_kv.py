036ff39ad88b6c2cb9e5c5ca6efd42c7a1c9d325
Pepesaur96 <jjvazquez96@gmail.com>
2024-01-04 12:17:49 -0400

    Task7.type-annotated to_kv function/copyNadja

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
-"""Type-annotated function to_kv that takes a string k and an int OR float v
-as arguments and returns a tuple. The first element of the tuple is the string k.
-The second element is the square of the int/float v and should be annotated as
-a float."""
-from typing import Union, Tuple
+"""type-annotated function takes a string
+and an int as arguments and returns a tuple."""
+from typing import Tuple, Union
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
-    """Return tuple with k and square of v"""
+    """type-annotated function takes a string
+    and an int as arguments and returns a tuple."""
     return (k, v * v)

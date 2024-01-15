f98587aed692bffca37eb9ea6a2c92b8113b58a4
Pepesaur96 <jjvazquez96@gmail.com>
2024-01-15 15:51:53 -0400

    Task1.Lint check, added const to all variable declarations

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -2,8 +2,8 @@ export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
   if (trueOrFalse) {
-      task = true;   // Renamed from task
-      task2 = false;  // Renamed from task2
+      const task = true;   // Renamed from task
+      const task2 = false;  // Renamed from task2
 }
   return [task, task2];
 }

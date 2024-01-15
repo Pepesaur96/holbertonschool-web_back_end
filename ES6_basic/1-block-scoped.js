edb308ce5074bae2ab514ff2774a69fbfb352e1c
Pepesaur96 <jjvazquez96@gmail.com>
2024-01-15 15:43:16 -0400

    Task1.Lint check, added const

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -2,8 +2,8 @@ export default function taskBlock(trueOrFalse) {
   var task = false;
   var task2 = true;
   if (trueOrFalse) {
-  var innerTask = true;   // Renamed from task
-  var innerTask2 = false;  // Renamed from task2
+  const innerTask = true;   // Renamed from task
+  const innerTask2 = false;  // Renamed from task2
 }
   return [task, task2];
 }

fcc274322bb15ad8995b9af2a7db0881ea1e3ae7
Pepesaur96 <jjvazquez96@gmail.com>
2024-01-15 15:46:25 -0400

    Task1.Lint check, added const to fisrt variable declaration.2

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -2,8 +2,8 @@ export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
   if (trueOrFalse) {
-  Task = true;   // Renamed from task
-  Task2 = false;  // Renamed from task2
+      Task = true;   // Renamed from task
+      Task2 = false;  // Renamed from task2
 }
   return [task, task2];
 }

379844a6260dab9fead717dd0ee469cbd6b2d06c
Pepesaur96 <jjvazquez96@gmail.com>
2024-01-15 15:31:11 -0400

    Task1.Removed if statement

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,9 +1,7 @@
 export default function taskBlock(trueOrFalse) {
   var task = false;
   var task2 = true;
-if (trueOrFalse) {
-  task = true;  // Removed 'var'
-  task2 = false;  // Removed 'var'
+//Removed if statement
 }
   return [task, task2];
 }

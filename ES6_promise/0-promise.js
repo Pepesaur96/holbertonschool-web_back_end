a95c74c2cd3276ab6a2a92193813ccd04bdaf8ba
Pepesaur96 <jjvazquez96@gmail.com>
2024-01-26 13:40:25 -0400

    Task0.Lint fix

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,8 +1,8 @@
 export default function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve('true');
-        reject(new Error(''));
-      });
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('true');
+      reject(new Error(''));
     });
+  });
 }

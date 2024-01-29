42ea8f7d0ab022c9cc7c1847fa312683a4012e71
Pepesaur96 <jjvazquez96@gmail.com>
2024-01-29 11:49:43 -0400

    Task2.Return list of objects by city

---
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,5 @@
+// A function that returns an array of objects who are located in a specific city.
+
+export default function getStudentsByLocation(students, city) {
+  return students.filter((student) => student.location === city);
+}

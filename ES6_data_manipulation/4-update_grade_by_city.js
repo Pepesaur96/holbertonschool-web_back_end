c14e24c50070bda34f9e6f58fbc74cb648383ca8
Pepesaur96 <jjvazquez96@gmail.com>
2024-01-29 12:16:35 -0400

    Task4.Upgrade and return students by city

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,11 @@
+// A function named updateStudentGradeByCity that returns an array of students for a specific city with their new grade.
+
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
+      const grade = gradeObj ? gradeObj.grade : 'N/A';
+      return { ...student, grade };
+    });
+}
